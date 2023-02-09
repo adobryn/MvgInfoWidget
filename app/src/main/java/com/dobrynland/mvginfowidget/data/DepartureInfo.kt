423050055d93fdbba749bee1a0fd0782b64391af
Alexandra Dobryn (ext.) <Alexandra.Dobryn@partner.bmwgroup.com>
2023-02-09 11:41:42 +0100

    Added all JSON fields in POJO class

--- app/src/main/java/com/dobrynland/mvginfowidget/data/DepartureInfo.kt
+++ app/src/main/java/com/dobrynland/mvginfowidget/data/DepartureInfo.kt
@@ -3,10 +3,18 @@ package com.dobrynland.mvginfowidget.data
 import com.google.gson.annotations.SerializedName
 
 data class DepartureInfo (
-    @SerializedName("departure_time") val departureTime: Int?,
+    @SerializedName("departureTime") val departureTime: Long?,
     @SerializedName("product") val product: String?,
     @SerializedName("label") val label: String?,
     @SerializedName("destination") val destination: String?,
+    @SerializedName("live") val live: Boolean?,
     @SerializedName("delay") val delay: Boolean?,
-    @SerializedName("cancelled") val cancelled: Boolean?
+    @SerializedName("cancelled") val cancelled: Boolean?,
+    @SerializedName("lineBackgroundColor") val lineBackgroundColor: String?,
+    @SerializedName("departureId") val departureId: String?,
+    @SerializedName("sev") val sev: Boolean?,
+    @SerializedName("platform") val platform: String?,
+    @SerializedName("stopPositionNumber") val stopPositionNumber: Int?,
+    @SerializedName("infoMessages") val infoMessages: List<String>?,
+    @SerializedName("departureTimeMinutes") val departureTimeMinutes: Int?
 )
